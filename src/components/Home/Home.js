ecbfbc28c5c8898d3fd69626be574987617b2fea
ashaydharmik <ashaydharmik807@gmail.com>
2023-11-01 13:12:09 +0530

    modal component added and notes container added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,50 +1,128 @@
-import React, { useState } from 'react'
-import "./home.scss"
-import bg from "../../assets/bg.png"
-import lock from "../../assets/lock.png"
-import Modal from '../Modal/Modal'
+import React, { useState, useEffect } from "react";
+import "./home.scss";
+import bg from "../../assets/bg.png";
+import lock from "../../assets/lock.png";
+import Modal from "../Modal/Modal";
+import arrow from "../../assets/Vector.png";
+
 const Home = () => {
-    const[showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
+  const [groups, setGroups] = useState([]);
+  const [selectedNoteName, setSelectedNoteName] = useState(null);
+  const [isInNotesBox, setIsInNotesBox] = useState(false);
 
-        const closeModal =()=>setShowModal(false);
+  const closeModal = () => setShowModal(false);
 
+  const createGroup = (notesName, selectedColor) => {
+    const notesIcon = notesName.substring(0, 2).toUpperCase();
+    const newGroup = {
+      name: notesName,
+      icon: notesIcon,
+      backgroundColor: selectedColor,
+    };
+    setGroups([...groups, newGroup]);
+  };
+
+  const handleNoteName = (nameOfNote) => {
+    setSelectedNoteName(nameOfNote);
+    setIsInNotesBox(true);
+
+    if (window.innerWidth < 768) {
+      document.querySelector(".menu").classList.add("menu-hidden-mobile");
+      document.querySelector(".notes-box").classList.add("notes-box-visible-mobile");
+    } else {
+      document.querySelector(".menu").classList.remove("menu-hidden-mobile");
+      document.querySelector(".notes-box").classList.remove("notes-box-visible-mobile");
+    }
+  };
+
+  const handleArrowClick = () => {
+    setIsInNotesBox(false);
+    if (window.innerWidth < 768) {
+      document.querySelector(".menu").classList.remove("menu-hidden-mobile");
+    }
+  };
 
   return (
     <>
-    <div className='container'>
-        <div className='menu'>
-            <div className='heading'>
-                <h1>Pocket Notes</h1>
-            </div>
-            <button className='add' onClick={()=> setShowModal(true)}>                
-                    <p id='symbol'>+</p>
-                    <p id='para'>Create Notes group</p>     
-            </button>
-           {showModal && <Modal closeModal={closeModal}/>}
-            <div className='notes'>
-                <div className='notes-details'>
-                    <div className='icon'>
-                        <p>CU</p>
-                        </div>
-                    <div className='name'>
-                        <p>Cuvette notes</p>
-                        </div>
+      <div className="container">
+        <div className="menu ">
+          <div className="heading">
+            <h1>Pocket Notes</h1>
+          </div>
+          <button className="add" onClick={() => setShowModal(true)}>
+            <p id="symbol">+</p>
+            <p id="para">Create Notes group</p>
+          </button>
+          {showModal && (
+            <Modal closeModal={closeModal} createGroup={createGroup} />
+          )}
+          <div className="notes">
+            {groups.map((note, index) => (
+              <div className="notes-details" key={index}>
+                <div
+                  className="icon"
+                  style={{ backgroundColor: note.backgroundColor }}
+                >
+                  <p>{note.icon}</p>
                 </div>
-            </div>
+                <div className="name">
+                  <p onClick={() => handleNoteName(note.name)}>{note.name}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className='content'>
-            <div className='center-content'>
-                <img src={bg} alt=''></img>
+        <div className="notes-box">
+          {isInNotesBox ? (
+            <div className="notebox-heading">
+              {selectedNoteName && groups.map((note, index) => {
+                if (note.name === selectedNoteName) {
+                  return (
+                    <>
+                    <div key={index} className="note-header">
+                      <p onClick={handleArrowClick}><img src={arrow} alt=""/></p>
+                      <div className="icon" style={{ backgroundColor: note.backgroundColor }}>
+                        <p>{note.icon}</p>
+                      </div>
+                      <h1>{note.name}</h1>
+                    </div>
+                    <div className="notes-written">
+                        <div className="time-date">
+                          <p>10:10 AM</p>
+                          <p>9 March 2023</p>
+                        </div>
+                        <div className="desc">
+                          <p>Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.</p>
+                        </div>
+                    </div>
+                    </>
+                  );
+                }
+                return null;
+              })}
+            </div>
+          ) : (
+            <div className="content">
+              <div className="center-content">
+                <img src={bg} alt=""></img>
                 <h2>Pocket Notes</h2>
-                <p>Send and receive messages without keeping your phone online. Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
+                <p>
+                  Send and receive messages without keeping your phone online.
+                  Use Pocket Notes on up to 4 linked devices and 1 mobile phone
+                </p>
+              </div>
+              <div className="bottom-content">
+                <p>
+                  <img src={lock} alt=""></img> end-to-end encrypted
+                </p>
+              </div>
             </div>
-            <div className='bottom-content'>
-                <p><img src={lock} alt=''></img> end-to-end encrypted</p>
-            </div>
+          )}
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,40 +1,114 @@
-import React from 'react'
-import "./modal.scss"
-const Modal = ({closeModal}) => {
+import React, { useEffect, useState } from "react";
+import "./modal.scss";
+const Modal = ({ closeModal, createGroup }) => {
+  const [selectedColor, setSelectedColor] = useState('purple');
+  //to store notes names
+  const[noteName, setNoteName] = useState("")
+ 
+  
+
+  const selectColors = (color) => {
+    if (selectedColor === color) {
+      setSelectedColor(null);
+    } else {
+      setSelectedColor(color);
+    }
+  };
+
+  const getBorderStyle = (color) => {
+    return selectedColor === color ? { border: "1px solid black" } : {};
+  };
+
+  const handleNameChange=(e)=>{
+    setNoteName(e.target.value); 
+  }
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    if (noteName && selectedColor) {
+      createGroup(noteName, selectedColor);
+  
+      // Retrieve the existing data from local storage or initialize an empty array
+      const existingNoteNames = JSON.parse(localStorage.getItem("storedNoteName")) || [];
+      
+  
+      // Update the data by adding the new note name
+      const updatedNoteNames = [...existingNoteNames, noteName];
+      // console.log(updatedNoteNames)
+      // Save the updated data back to local storage
+      localStorage.setItem("storedNoteName", JSON.stringify(updatedNoteNames));
+    }
+    closeModal();
+  };
+  
+ 
+  
   return (
     <>
-    <div className='modal-wrapper'></div>
-      <div className='modal-container'>
-        <div className='modal'>
-            <div className='title'>
-                <h1>Create New Notes group</h1>
-            </div>
-            <div className='form'>
-                <form >
-                    <label>Group Name</label>
-                    <input type='text' placeholder='Enter your group name...' required></input><br/>
-                    <div className='color-box'>
-                    <label>Choose colour</label>
+      <div className="modal-wrapper"></div>
+      <div className="modal-container">
+        <div className="modal">
+          <div className="title">
+            <h1>Create New Notes group</h1>
+          </div>
+          <div className="form">
+            <form onSubmit={handleForm}>
+              <label>Group Name</label>
+              <input
+                type="text"
+                placeholder="Enter your group name..."
+                required
+                onChange={handleNameChange}
+              ></input>
+              <br />
+              <div className="color-box">
+                <label>Choose colour</label>
 
-                    <div className='colors'>
-                <p id='purple'></p>
-                    <p id='pink'></p>
-                    <p id='green'></p>
-                    <p id='orange'></p>
-                    <p id='blue'></p>
-                    <p id='skyblue'></p>
-                    </div>
-                    </div>
-                   <div className='buttons'>
-
-                    <button type='submit' onClick={closeModal}>Create</button>
-                   </div>
-                </form>
-            </div>
+                <div className="colors">
+                  <p
+                    id="purple"
+                    onClick={() => selectColors("#b38bfa")}
+                    style={getBorderStyle("#b38bfa")}
+                    
+                  ></p>
+                  <p
+                    id="pink"
+                    onClick={() => selectColors("#ff79f2")}
+                    style={getBorderStyle("#ff79f2")}
+                  ></p>
+                  <p
+                    id="green"
+                    onClick={() => selectColors("#43e6fc")}
+                    style={getBorderStyle("#43e6fc")}
+                  ></p>
+                  <p
+                    id="orange"
+                    onClick={() => selectColors("#f19576")}
+                    style={getBorderStyle("#f19576")}
+                  ></p>
+                  <p
+                    id="blue"
+                    onClick={() => selectColors("#0047ff")}
+                    style={getBorderStyle("#0047ff")}
+                  ></p>
+                  <p
+                    id="skyblue"
+                    onClick={() => selectColors("#6691ff")}
+                    style={getBorderStyle("#6691ff")}
+                  ></p>
+                </div>
+              </div>
+              <div className="buttons">
+                <button type="submit" >
+                  Create
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
